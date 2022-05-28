f74d8578662eea7fdc0e9d480bf9abfa9bb26c59
saakibislam <sakib.cseacademic@gmail.com>
2022-05-28 22:16:45 +0600

    OTP Verification Added

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,24 @@
 const video = document.getElementById('videoInput')
 const capture = document.getElementById('capture')
 const headTitle = document.getElementById('head-title')
+const proceedOtpButton = document.getElementById('proceed-otp')
+const phoneField = document.getElementById('phone-number')
+const otpContainer = document.getElementById('otp-container')
+const otpField = otpContainer.children[1]
+
 let interval;
-
-
-setTimeout(() => {
-    capture.removeAttribute('disabled');
-    capture.innerText = 'Verify Now';
-}, 5000);
-
+let myOTP = null;
 
 Promise.all([
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('/models') //heavier/accurate version of tiny face detector
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('./models') //heavier/accurate version of tiny face detector
 ]).then(start)
+
 
 function start() {
     //document.body.append('Models Loaded')
+
     //DEPRECATED
     navigator.getUserMedia(
         { video: {} },
@@ -27,7 +28,14 @@ function start() {
 
     //video.src = '../videos/speech.mp4'
     console.log('video added')
+
+    // Replacing Button
+    capture.innerText = 'Verify Now';
+    capture.classList.replace("btn-warning", "btn-success")
+    capture.removeAttribute('disabled')
+
     recognizeFaces()
+
 }
 
 async function recognizeFaces() {
@@ -36,7 +44,7 @@ async function recognizeFaces() {
     //console.log(labeledDescriptors)
     const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.5) //dissimilarity range
 
-
+    // Verify Now Button Event Listener
     capture.addEventListener('click', async () => {
         console.log('Playing')
         const canvas = faceapi.createCanvasFromMedia(video)
@@ -78,9 +86,10 @@ async function recognizeFaces() {
     })
 }
 
+// Load Images of Stored Users
 function loadLabeledImages() {
     //const labels = ['Black Widow', 'Captain America', 'Hawkeye' , 'Jim Rhodes', 'Tony Stark', 'Thor', 'Captain Marvel']
-    const labels = ['P541000', 'C171026', '1111122222', '123456'] // for WebCam
+    const labels = ['P541000', 'C171026'] // for WebCam
     return Promise.all(
         labels.map(async (label) => {
             const descriptions = []
@@ -96,6 +105,7 @@ function loadLabeledImages() {
     )
 }
 
+// What to Do if fails / success
 function finalExecution(preAns) {
     //mine
     let shortenedAns = [];
@@ -105,15 +115,19 @@ function finalExecution(preAns) {
     let ans = mostFrequent(shortenedAns);
     console.log("Verified: " + ans);
     if (ans === -1) {
-        headTitle.innerText = "Verification Failed"
-        headTitle.classList.add("heading-danger")
         console.log("Not verified,Try Again")
-        alert("Verification Failed, Please Retry")
-        window.location.reload();
+        headTitle.innerText = "Verification Failed"
+        headTitle.classList.add("text-danger")
+        capture.innerText = "Retry"
+        capture.classList.replace("btn-success", "btn-danger")
+        capture.addEventListener("click", () => {
+            window.location.reload();
+        })
+        proceedOtpButton.classList.remove('d-none')
     }
     else {
         headTitle.innerText = 'Verification Successful';
-        headTitle.classList.add("heading-success");
+        headTitle.classList.add("text-success");
         console.log('The photo is of: ' + ans);
         clearInterval(interval);
         //send user to vote page
@@ -124,6 +138,7 @@ function finalExecution(preAns) {
     //mine->ends here
 }
 
+// Result Finding
 function mostFrequent(arr) {
     let mf = 1, m = 0, item, unknown = "unknown";
 
@@ -143,4 +158,49 @@ function mostFrequent(arr) {
     if (mf > 5) return item;
     else return -1;
     //alert(item + " ( " + mf + " times ) ");
+}
+
+// Resetting OTP Phone number field
+function dismissOTP() {
+    phoneField.removeAttribute('disabled')
+    phoneField.value = ''
+    document.getElementsByClassName('modal-footer')[0].children[1].removeAttribute('disabled')
+    otpContainer.classList.add('d-none')
+}
+
+// Request for OTP
+function handleOTP() {
+    const number = phoneField.value;
+    // console.log('phone number: ' + number)
+
+    if (number === '') {
+        alert("Please provide a number")
+
+    } else {
+        // console.log('handling otp request')
+        phoneField.setAttribute('disabled', '')
+        document.getElementsByClassName('modal-footer')[0].children[1].setAttribute('disabled', '')
+        otpContainer.classList.remove('d-none')
+
+        // OTP SEND
+        fetch('http://localhost:5000/phoneVerification', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ phoneNumber: number })
+        })
+            .then(res => res.json())
+            .then(async data => myOTP = await data.sentOtp)
+    }
+}
+
+// Verify OTP
+function verifyOTP() {
+    const inputOtp = Number(otpField.value)
+    if (inputOtp === myOTP) {
+        window.location.replace('https://online-voting-system.web.app/vote')
+    } else {
+        alert('Invalid OTP. Please type correct OTP code.')
+    }
 }
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
-const fs = require('fs').promises
+const messagebird = require('messagebird')('Fox9jJABmPIJ0FrIKSmMJtNZR')
+// const fs = require('fs').promises
 
 const app = express()
 const port = process.env.PORT || 8989
@@ -8,6 +9,15 @@ const port = process.env.PORT || 8989
 const basePath = path.join(__dirname, '../public')
 
 app.use(express.static(basePath))
+
+/* 
+async function getLabels(folderName) {
+    const storeFiles = await fs.readdir(folderName);
+    return storeFiles
+}
+
+getLabels('./public/labeled_images')
+ */
 
 //start express server
 app.listen(port, () => {
